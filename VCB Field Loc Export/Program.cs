c0946e0f924c2df4d12b5b30e0e2aea49db946b3
James Fairweather <james.fairweather@blackbirdinteractive.com>
2024-03-29 09:31:41 -0700

    Project created from template

---
+++ VCB Field Loc Export/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCB_Field_Loc_Export
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
