52deb65ef20149dce430a5f699de0aef40ad5985
James Fairweather <james.fairweather@blackbirdinteractive.com>
2024-04-12 01:03:55 -0700

    Make the FetchEvents step into its own function.

--- VCB Field Loc Export/Program.cs
+++ VCB Field Loc Export/Program.cs
@@ -12,6 +12,31 @@ using static System.Net.Mime.MediaTypeNames;
 
 namespace VcbFieldExport
 {
+    enum EventType
+    {
+        Practice,
+        LeagueControlledGame,
+        TeamControlledGame,
+    };
+
+    class VcbFieldEvent
+    {
+        public VcbFieldEvent(EventType _eventType, string _homeTeam, string _visitingTeam, DateTime start, DateTime end)
+        {
+            eventType = _eventType;
+            homeTeam = _homeTeam;
+            visitingTeamOrDescription = _visitingTeam;
+            startTime = start;
+            endTime = end;
+        }
+
+        public EventType eventType;
+        public string homeTeam;
+        public string visitingTeamOrDescription;
+        public DateTime startTime;
+        public DateTime endTime;
+    };
+
     internal partial class Program
     {
         [GeneratedRegex(@"(\d+:\d+ (AM|PM))( - (\d+:\d+ (AM|PM)))?")]
@@ -35,6 +60,8 @@ namespace VcbFieldExport
 
         static int Main(string[] args)
         {
+            int returnValue = 0;
+
             string sessionId = args[0];
 
             if (string.IsNullOrEmpty(sessionId))
@@ -43,6 +70,15 @@ namespace VcbFieldExport
                 return -1;
             }
 
+            foreach (int locationId in locationIds.Keys) {
+                List<VcbFieldEvent> newEvents = FetchEvents(sessionId, locationId);
+            }
+
+            return returnValue;
+        }
+
+        static List<VcbFieldEvent> FetchEvents(string sessionId, int locationId) {
+
             string eventPattern = @"<tr.+>\s" +
             @"<td (colspan=""2"" )?class=""l-Schedule__col-game-event"">\s(<a .+>)?(?<EventName>[^<]+)(</a>)?\s</td>\s" +
             @"(<td class=""l-Schedule__col-result"">\s(.+\s){3}</td>\s)?<td class=""l-Schedule__col-date"">\s(?<Date>.+)\s</td>\s" +
@@ -50,109 +86,106 @@ namespace VcbFieldExport
 
             Regex eventRegex = new(eventPattern, RegexOptions.ExplicitCapture | RegexOptions.NonBacktracking | RegexOptions.Compiled);
 
+            List<VcbFieldEvent> events = new List<VcbFieldEvent>();
+
             using HttpClient client = new();
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Add("Cookie", $"_ts_session={sessionId}");
 
-            foreach (int locationId in locationIds.Keys) {
-                Console.WriteLine($"Processing events for location {locationIds[locationId]}");
-                int currentPage = 1;
+            Console.WriteLine($"Processing events for location {locationIds[locationId]}");
+            int currentPage = 1;
 
-                string responseHtml = GetVcbFieldEvents(client, locationId, currentPage);
+            string responseHtml = GetVcbFieldEvents(client, locationId, currentPage);
 
-                int contentStartIndex = responseHtml.IndexOf("<div id=\"content\">");
+            int contentStartIndex = responseHtml.IndexOf("<div id=\"content\">");
 
-                if (contentStartIndex == -1) {
-                    Console.WriteLine("Error - did not find the expected event table start tag - check the returned HTML content and update the script");
-                    return -1;
-                }
-
-                Match m = PaginationRegex().Match(responseHtml, contentStartIndex);
-
-                int expectedPageCount = m.Success ? Int32.Parse(m.Groups[1].Value) : 1;
-
-                Console.WriteLine($"Expecting {expectedPageCount} pages of events for this location.");
-
-                StreamWriter outputFile = new($"{locationIds[locationId]}.csv");
-                outputFile.WriteLine("Event/Game,Home Team,Event title or Visiting Team,Start Time,End Time");
-
-                while (currentPage <= expectedPageCount) {
-                    Console.WriteLine($"Processing page {currentPage} ...");
-
-                    int eventCount = 0;
-                    Match eventMatches = eventRegex.Match(responseHtml, contentStartIndex);
-
-                    while (eventMatches.Success) {
-                        DateTime startTime, endTime;
-
-                        Match timeMatch = TimeRegex().Match(eventMatches.Groups["Time"].Value);
-
-                        if (timeMatch.Success) {
-                            startTime = DateTime.Parse(eventMatches.Groups["Date"].Value) + DateTime.Parse(timeMatch.Groups[1].Value).TimeOfDay;
-
-                            if (!string.IsNullOrEmpty(timeMatch.Groups[4].Value)) {
-                                endTime = DateTime.Parse(eventMatches.Groups["Date"].Value) + DateTime.Parse(timeMatch.Groups[4].Value).TimeOfDay;
-                            }
-                            else {
-                                endTime = startTime.AddHours(3);
-                            }
-
-                            string gameOrEventString = eventMatches.Groups["EventName"].Value.Trim();
-
-                            // Event: VCB 13U A Giants: Practice,  // Events will have a colon that separates
-                            // Game (league controlled): VCB 13U A Red Sox at VCB 13U A Diamondbacks  // Games will have a <visiting team> "at" <VCB team>
-                            // Game (team-controlled): VCB Expos 15U AA - Blue vs.Vic - Layritz(Team Controlled)
-
-                            Match teamEvent = TeamEventRegex().Match(gameOrEventString);
-
-                            if (teamEvent.Success) {
-                                outputFile.WriteLine($"Team event,{teamEvent.Groups["TeamName"].Value},{teamEvent.Groups["EventTitle"].Value},{startTime},{endTime}");
-                            }
-                            else {
-                                Match leagueControlledGame = LeagueControlledGameRegex().Match(gameOrEventString);
-
-                                if (leagueControlledGame.Success) {
-                                    outputFile.WriteLine($"Game,{leagueControlledGame.Groups["HomeTeamName"].Value},{leagueControlledGame.Groups["VisitorTeamName"].Value},{startTime},{endTime}");
-                                }
-                                else {
-                                    Match teamControlledGame = TeamControlledGameRegex().Match(gameOrEventString);
-                                    if (teamControlledGame.Success) {
-                                        outputFile.WriteLine($"Game,{teamControlledGame.Groups["VcbTeamName"].Value},{teamControlledGame.Groups["OpponentName"].Value},{startTime},{endTime}");
-                                    }
-                                    else {
-                                        Console.WriteLine($"Warning: unable to parse the event string for the event {gameOrEventString}.  Check the returned HTML");
-                                        outputFile.WriteLine($"Event,{gameOrEventString},,{startTime},{endTime}");
-                                    }
-                                }
-                            }
-
-                            eventCount++;
-                        }
-                        else {
-                            Console.WriteLine($"Warning: Event {eventMatches.Groups["EventName"].Value.Trim()} on {eventMatches.Groups["Date"].Value} has an unparsable time value ({eventMatches.Groups["Time"].Value}).  Skipping...");
-                        }
-
-                        eventMatches = eventMatches.NextMatch();
-                    }
-
-                    Console.WriteLine($"Parsed {eventCount} events from this page.");
-
-                    currentPage++;
-
-                    responseHtml = GetVcbFieldEvents(client, locationId, currentPage);
-                    contentStartIndex = responseHtml.IndexOf("<div id=\"content\">");
-
-                    if (contentStartIndex == -1)
-                    {
-                        Console.WriteLine("Error - did not find the expected event table start tag - check the returned HTML content and update the script");
-                        return -1;
-                    }
-                }
-
-                outputFile.Close();
+            if (contentStartIndex == -1) {
+                throw new Exception("Error - did not find the expected event table start tag - check the returned HTML content and update the script");
             }
 
-            return 0;
+            Match m = PaginationRegex().Match(responseHtml, contentStartIndex);
+
+            int expectedPageCount = m.Success ? Int32.Parse(m.Groups[1].Value) : 1;
+
+            Console.WriteLine($"Expecting {expectedPageCount} pages of events for this location.");
+
+            StreamWriter outputFile = new($"{locationIds[locationId]}.csv");
+            outputFile.WriteLine("Event/Game,Home Team,Event title or Visiting Team,Start Time,End Time");
+
+            while (currentPage <= expectedPageCount) {
+                Console.WriteLine($"Processing page {currentPage} ...");
+
+                int eventCount = 0;
+                Match eventMatches = eventRegex.Match(responseHtml, contentStartIndex);
+
+                while (eventMatches.Success) {
+                    DateTime startTime, endTime;
+
+                    Match timeMatch = TimeRegex().Match(eventMatches.Groups["Time"].Value);
+
+                    if (timeMatch.Success) {
+                        startTime = DateTime.Parse(eventMatches.Groups["Date"].Value) + DateTime.Parse(timeMatch.Groups[1].Value).TimeOfDay;
+
+                        if (!string.IsNullOrEmpty(timeMatch.Groups[4].Value)) {
+                            endTime = DateTime.Parse(eventMatches.Groups["Date"].Value) + DateTime.Parse(timeMatch.Groups[4].Value).TimeOfDay;
+                        }
+                        else {
+                            endTime = startTime.AddHours(3);
+                        }
+
+                        string gameOrEventString = eventMatches.Groups["EventName"].Value.Trim();
+
+                        // Event: VCB 13U A Giants: Practice,  // Events will have a colon that separates
+                        // Game (league controlled): VCB 13U A Red Sox at VCB 13U A Diamondbacks  // Games will have a <visiting team> "at" <VCB team>
+                        // Game (team-controlled): VCB Expos 15U AA - Blue vs.Vic - Layritz(Team Controlled)
+
+                        Match teamEvent = TeamEventRegex().Match(gameOrEventString);
+
+                        if (teamEvent.Success) {
+                            events.Add(new VcbFieldEvent(EventType.Practice, teamEvent.Groups["TeamName"].Value,teamEvent.Groups["EventTitle"].Value,startTime,endTime));
+                        }
+                        else {
+                            Match leagueControlledGame = LeagueControlledGameRegex().Match(gameOrEventString);
+
+                            if (leagueControlledGame.Success) {
+                                events.Add(new VcbFieldEvent(EventType.LeagueControlledGame, leagueControlledGame.Groups["HomeTeamName"].Value,leagueControlledGame.Groups["VisitorTeamName"].Value,startTime,endTime));
+                            }
+                            else {
+                                Match teamControlledGame = TeamControlledGameRegex().Match(gameOrEventString);
+                                if (teamControlledGame.Success) {
+                                    events.Add(new VcbFieldEvent(EventType.TeamControlledGame, teamControlledGame.Groups["VcbTeamName"].Value,teamControlledGame.Groups["OpponentName"].Value,startTime,endTime));
+                                }
+                                else {
+                                    Console.WriteLine($"Warning: unable to parse the event string for the event {gameOrEventString}.  Check the returned HTML");
+                                    events.Add(new VcbFieldEvent(EventType.Practice, gameOrEventString, "", startTime, endTime));
+                                }
+                            }
+                        }
+
+                        eventCount++;
+                    }
+                    else {
+                        Console.WriteLine($"Warning: Event {eventMatches.Groups["EventName"].Value.Trim()} on {eventMatches.Groups["Date"].Value} has an unparsable time value ({eventMatches.Groups["Time"].Value}).  Skipping...");
+                    }
+
+                    eventMatches = eventMatches.NextMatch();
+                }
+
+                Console.WriteLine($"Parsed {eventCount} events from this page.");
+
+                currentPage++;
+
+                responseHtml = GetVcbFieldEvents(client, locationId, currentPage);
+                contentStartIndex = responseHtml.IndexOf("<div id=\"content\">");
+
+                if (contentStartIndex == -1) {
+                    throw new Exception("Error - did not find the expected event table start tag - check the returned HTML content and update the script");
+                }
+            }
+
+            outputFile.Close();
+
+            return events;
         }
 
         static string GetVcbFieldEvents(HttpClient client, int locationId, int page)
