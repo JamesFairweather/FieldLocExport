9d1c3a54891b442ab443132f0cad4045c9fc5c65
James Fairweather <james.fairweather@blackbirdinteractive.com>
2024-04-12 00:33:05 -0700

    Add the ability to insert a calendar event.  Easy!

--- CalendarQuickStart/Program.cs
+++ CalendarQuickStart/Program.cs
@@ -3,6 +3,7 @@ using Google.Apis.Calendar.v3;
 using Google.Apis.Calendar.v3.Data;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
+using System.Globalization;
 
 // Source:
 // https://stackoverflow.com/questions/55103032/how-to-create-an-event-in-google-calendar-using-c-sharp-and-google-api
@@ -13,7 +14,7 @@ namespace CalendarQuickstart
     {
         // If modifying these scopes, delete your previously saved credentials
         // at ~/.credentials/calendar-dotnet-quickstart.json
-        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        static string[] Scopes = { CalendarService.Scope.Calendar };
         static string ApplicationName = "Google Calendar API .NET Quickstart";
 
         static void Main(string[] args)
@@ -27,7 +28,7 @@ namespace CalendarQuickstart
                 // automatically when the authorization flow completes for the first time.
                 string credPath = "token.json";
                 credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
+                    GoogleClientSecrets.FromStream(stream).Secrets,
                     Scopes,
                     "user",
                     CancellationToken.None,
@@ -42,34 +43,51 @@ namespace CalendarQuickstart
                 ApplicationName = ApplicationName,
             });
 
-            // Define parameters of request.
-            EventsResource.ListRequest request = service.Events.List("primary");
-            request.TimeMin = DateTime.Now;
-            request.ShowDeleted = false;
-            request.SingleEvents = true;
-            request.MaxResults = 10;
-            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+            //// Define parameters of request.
+            //EventsResource.ListRequest request = service.Events.List("primary");
+            //request.TimeMin = DateTime.Now;
+            //request.ShowDeleted = false;
+            //request.SingleEvents = true;
+            //request.MaxResults = 10;
+            //request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
 
-            // List events.
-            Events events = request.Execute();
-            Console.WriteLine("Upcoming events:");
-            if (events.Items != null && events.Items.Count > 0)
-            {
-                foreach (var eventItem in events.Items)
-                {
-                    string when = eventItem.Start.DateTime.ToString();
-                    if (String.IsNullOrEmpty(when))
-                    {
-                        when = eventItem.Start.Date;
-                    }
-                    Console.WriteLine("{0} ({1})", eventItem.Summary, when);
-                }
-            }
-            else
-            {
-                Console.WriteLine("No upcoming events found.");
-            }
-            Console.Read();
+            //// List events.
+            //Events events = request.Execute();
+            //Console.WriteLine("Upcoming events:");
+            //if (events.Items != null && events.Items.Count > 0)
+            //{
+            //    foreach (var eventItem in events.Items)
+            //    {
+            //        string when = eventItem.Start.DateTime.ToString();
+            //        if (String.IsNullOrEmpty(when))
+            //        {
+            //            when = eventItem.Start.Date;
+            //        }
+            //        Console.WriteLine("{0} ({1})", eventItem.Summary, when);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("No upcoming events found.");
+            //}
+            //Console.Read();
+
+            Event googleCalendarEvent = new();
+
+            googleCalendarEvent.Start = new EventDateTime()
+            { DateTimeDateTimeOffset = new DateTime(2024, 4, 15, 10, 0, 0) };
+
+            googleCalendarEvent.End = new EventDateTime()
+            { DateTimeDateTimeOffset = new DateTime(2024, 4, 15, 11, 0, 0) };
+
+            googleCalendarEvent.Summary = "New Event from api";
+            googleCalendarEvent.Description = "Description of my event";
+
+            var calendarId = "primary"; //Always primary.
+
+            Event result = service.Events.Insert(googleCalendarEvent, calendarId).Execute();
+
+            Console.WriteLine("Event created: %s\n", result.HtmlLink);
         }
     }
 }
