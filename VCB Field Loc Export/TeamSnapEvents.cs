JamesFairweather/FieldLocExport

commit fa3315eae9e10f1545e171a1793b43218f45bdec

Fixed all the warnings about dereferencing potential null types.

VCB Field Loc Export/TeamSnapEvents.cs
@@ -1,9 +1,54 @@
 ﻿using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using System.Text.RegularExpressions;
+using Google.Apis.Util;
 
 namespace VcbFieldExport
 {
+    public class Datum
+    {
+        public Datum()
+        {
+            name = string.Empty;
+            value = string.Empty;
+        }
+
+        public string name { get; set; }
+        public string value { get; set; }
+    }
+
+    public class Item
+    {
+        Item()
+        {
+            data = new();
+        }
+
+        public List<Datum> data { get; set; }
+    }
+
+    public class Collection
+    {
+        public Collection()
+        {
+            version = string.Empty;
+            items = new();
+        }
+        public string version { get; set; }
+        public List<Item> items { get; set; }
+    }
+
+    public class TeamSnapResponseRoot
+    {
+        public TeamSnapResponseRoot()
+        {
+            collection = new();
+        }
+
+        public Collection collection { get; set; }
+    }
+
+
     class TeamSnapEvents
     {
         public TeamSnapEvents(StreamWriter logger)
@@ -59,53 +104,25 @@ namespace VcbFieldExport
             {
                 string jsonResponse = client.GetStringAsync($"https://api.teamsnap.com/v3/events/search?location_id={teamSnapLocationId}&started_after={DateTime.Today:O}").Result;
 
-                JObject? jsonRoot = JsonConvert.DeserializeObject(jsonResponse) as JObject;
+                TeamSnapResponseRoot jsonRoot = JsonConvert.DeserializeObject<TeamSnapResponseRoot>(jsonResponse) ?? new();
 
-                JArray eventList = jsonRoot["collection"]["items"] as JArray;
-
-                Dictionary<JValue, string> teamSnapEventFields = new Dictionary<JValue, string> {
-                    { (JValue)"location_name", "" },
-                    { (JValue)"start_date", "" },
-                    { (JValue)"end_date", "" },
-                    { (JValue)"is_game", "" },                 // boolean
-                    { (JValue)"is_league_controlled", "" },    // boolean
-                    { (JValue)"is_canceled", "" },             // boolean
-                    { (JValue)"game_type", "" },               // Home/Away
-                    { (JValue)"formatted_title", "" },
-                    { (JValue)"formatted_title_for_multi_team", "" },
-                    { (JValue)"opponent_name", "" },
-                };
-
-                foreach (JObject e in eventList)
+                foreach (Item e in jsonRoot.collection.items)
                 {
-                    JArray eventInfo = e["data"] as JArray;
-
-                    // store all the useful properties in a dictionary
-                    foreach (JObject prop in eventInfo)
-                    {
-                        JValue propName = (JValue)prop["name"];
-                        if (teamSnapEventFields.ContainsKey(propName))
-                        {
-                            teamSnapEventFields[propName] = prop["value"].ToString();
-                        }
-                    }
-
-                    if (teamSnapEventFields[(JValue)"is_canceled"] == "True")
+                    if (e.data.Find(x => x.name == "is_cancelled")?.value == "true")
                     {
                         continue;   // skip canceled events
                     }
 
-                    string location = teamSnapEventFields[(JValue)"location_name"];
-                    DateTime startTime = DateTime.Parse(teamSnapEventFields[(JValue)"start_date"]);
+                    string location = e.data.Find(x => x.name == "location_name")?.value ?? "TBD";
+                    DateTime startTime = DateTime.Parse(e.data.Find(x => x.name == "start_date")?.value ?? string.Empty).ToUniversalTime();
                     startTime = startTime.AddSeconds(-startTime.Second);    // sometimes TeamSnap events have non-zero seconds values, not sure why
-                    string endDateString = teamSnapEventFields[(JValue)"end_date"];
-                    DateTime endTime = string.IsNullOrEmpty(endDateString) ? startTime.AddHours(2) : DateTime.Parse(endDateString);
+                    string? endDateString = e.data.Find(x => x.name == "start_date")?.value;
+                    DateTime endTime = (endDateString != null) ? DateTime.Parse(endDateString).ToUniversalTime() : startTime.AddHours(2);
                     endTime = endTime.AddSeconds(-endTime.Second);
-                    string formatted_title = teamSnapEventFields[(JValue)"formatted_title"];
-                    string formatted_title_for_multi_team = teamSnapEventFields[(JValue)"formatted_title_for_multi_team"];
-                    string opponent_name = teamSnapEventFields[(JValue)"opponent_name"];
-                    bool homeGame = teamSnapEventFields[(JValue)"game_type"] == "Home";
-                    bool leagueControlledGame = teamSnapEventFields[(JValue)"is_league_controlled"] == "True";
+                    string formatted_title = e.data.Find(x => x.name == "formatted_title")?.value ?? string.Empty;
+                    string formatted_title_for_multi_team = e.data.Find(x => x.name == "formatted_title_for_multi_team")?.value ?? string.Empty;
+                    string opponent_name = e.data.Find(x => x.name == "opponent_name")?.value ?? string.Empty;
+                    bool leagueControlledGame = e.data.Find(x => x.name == "is_league_controlled")?.value == "true";
 
                     int index = formatted_title_for_multi_team.IndexOf(formatted_title);
                     if (index == -1)
@@ -115,15 +132,17 @@ namespace VcbFieldExport
                     string thisTeam = formatted_title_for_multi_team.Remove(index, formatted_title.Length).Trim();
                     string homeTeam = string.Empty;
                     string visitingTeam = string.Empty;
-                    bool addGameToEventList = true;
 
-                    if (teamSnapEventFields[(JValue)"is_game"] == "True")
+                    if (e.data.Find(x => x.name == "is_game")?.value == "true")
                     {
-                        if (homeGame)
+                        bool addGameToEventList;
+
+                        if (e.data.Find(x => x.name == "game_type")?.value == "Home")
                         {
                             // always add home games.
                             homeTeam = thisTeam;
                             visitingTeam = opponent_name;
+                            addGameToEventList = true;
                         }
                         else
                         {
@@ -131,23 +150,20 @@ namespace VcbFieldExport
                             homeTeam = opponent_name;
                             visitingTeam = thisTeam;
 
-                            if (leagueControlledGame || (homeTeam.StartsWith("VCB") && homeTeam != "VCB 15U TBD"))
-                            {
-                                // If the game is controlled by the league, we want to skip adding beause it will be
-                                // added by the home team.
+                            // If the game is controlled by the league, we want to skip adding beause it will be
+                            // added by the home team.
 
-                                // OR, if this is the visiting team's event for a game where the home team is also a VCB team,
-                                // ignore it because the game will be added by the home team's entry.  We only want to
-                                // add this entry if this is a team-controlled game where the home team is NOT a VCB team.
-                                // There are a few games being played on VCB fields where the VCB team is the visiting
-                                // team.  Assignr has these games of course, and we want TeamSnap to show them too.
-                                // Except we still want to add the game if the home team is "VCB 15U TBD" because these are
-                                // Girls team games where their opponent is still to be decided.
-                                addGameToEventList = false;
-                            }
+                            // OR, if this is the visiting team's event for a game where the home team is also a VCB team,
+                            // ignore it because the game will be added by the home team's entry.  We only want to
+                            // add this entry if this is a team-controlled game where the home team is NOT a VCB team.
+                            // There are a few games being played on VCB fields where the VCB team is the visiting
+                            // team.  Assignr has these games of course, and we want TeamSnap to show them too.
+                            // Except we still want to add the game if the home team is "VCB 15U TBD" because these are
+                            // Girls team games where their opponent is still to be decided.
+                            addGameToEventList = !((leagueControlledGame || (homeTeam.StartsWith("VCB") && homeTeam != "VCB 15U TBD")));
                         }
 
-                        string division = string.Empty;
+                        string? division = null;
 
                         Match match = teamRegex.Match(thisTeam);
 
@@ -180,7 +196,7 @@ namespace VcbFieldExport
                             }
                         }
 
-                        if (string.IsNullOrEmpty(division)) {
+                        if (division == null) {
                             throw new Exception($"Unable to extract division information for team {thisTeam}");
                         }
 
@@ -190,7 +206,7 @@ namespace VcbFieldExport
                             && !visitingTeam.Contains("TBD")) {
 
                             // Check that a game between two VCB teams is in _both_ teams' TeamSnap schedules
-                            VcbFieldEvent oppositeGameFound = nonLeagueUnmatchedGamesBetweenVcbTeams.Find(e => e.location == location && e.startTime == startTime && e.homeTeam == homeTeam && e.visitingTeam == visitingTeam);
+                            VcbFieldEvent? oppositeGameFound = nonLeagueUnmatchedGamesBetweenVcbTeams.Find(e => e.location == location && e.startTime == startTime && e.homeTeam == homeTeam && e.visitingTeam == visitingTeam);
 
                             if (oppositeGameFound == null) {
                                 // First instance of this game.  We should find another one in their opponent's TeamSnap schedule later

VCB Field Loc Export/AssignrEvents.cs
@@ -1,5 +1,7 @@
 ﻿using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
+using static Google.Apis.Requests.BatchRequest;
+using System.Xml.Linq;
 
 namespace VcbFieldExport
 {
@@ -42,6 +44,64 @@ namespace VcbFieldExport
 
         [JsonProperty("client_secret")]
         public string Secret { get; set; }
+    }
+
+    internal class Game
+    {
+        public Game()
+        {
+            start_time = new();
+            age_group = string.Empty;
+            home_team = string.Empty;
+            away_team = string.Empty;
+            game_type = string.Empty;
+            cancelled = false;
+            _embedded = new();
+        }
+        public DateTime start_time { get; set; }
+        public string age_group { get; set; }
+        public string home_team { get; set; }
+        public string away_team { get; set; }
+        public string game_type { get; set; }
+        public bool cancelled { get; set; }
+        public Embedded _embedded { get; set; }
+    }
+
+
+    internal class Page
+    {
+        public int pages { get; set; }
+    }
+
+    internal class Venue
+    {
+        public Venue()
+        {
+            name = string.Empty;
+        }
+        public string name { get; set; }
+    }
+
+    internal class Embedded
+    {
+        public Embedded() {
+            games = new();
+            venue = new();
+        }
+        public List<Game> games { get; set; }
+        public Venue venue { get; set; }
+    }
+
+    internal class AssignrResponseRoot
+    {
+        public AssignrResponseRoot()
+        {
+            page = new();
+            _embedded = new();
+        }
+
+        public Page page { get; set; }
+        public Embedded _embedded { get; set; }
     }
 
     public class AssignrEvents
@@ -122,44 +182,38 @@ namespace VcbFieldExport
                     throw new Exception("Failed to retrieve game information from Assignr");
                 }
 
-                string result = gamesResponse.Content.ReadAsStringAsync().Result;
+                string jsonResponse = gamesResponse.Content.ReadAsStringAsync().Result;
 
                 JsonSerializerSettings settings = new();
                 settings.DateParseHandling = DateParseHandling.None;
-                JObject? jsonRoot = JsonConvert.DeserializeObject(result, settings) as JObject;
+                AssignrResponseRoot jsonRoot = JsonConvert.DeserializeObject<AssignrResponseRoot>(jsonResponse) ?? new();
 
                 if (jsonRoot == null) {
                     throw new Exception("Unexpected response from Assignr service");
                 }
 
-                totalPages = int.Parse(jsonRoot["page"]["pages"].ToString());
+                totalPages = jsonRoot.page.pages;
 
-                JArray gameList = jsonRoot["_embedded"]["games"] as JArray;
-
-                foreach (JObject game in gameList)
+                foreach (Game game in jsonRoot._embedded.games)
                 {
-                    string startTime = game["start_time"].ToString();
-                    string ageGroup = game["age_group"].ToString();
-                    string homeTeam = game["home_team"].ToString();
-                    string awayTeam = game["away_team"].ToString();
-                    string assignrVenue = game["_embedded"]["venue"]["name"].ToString();
-                    string gameType = (game["game_type"] ?? string.Empty).ToString();
-                    bool isCancelled = (bool)game["cancelled"];
+                    string homeTeam = game.home_team;
+                    string awayTeam = game.away_team;
+                    string age_group = game.age_group ?? string.Empty;
 
-                    if (isCancelled) {
+                    if (game.cancelled) {
                         continue;
                     }
 
-                    if (gameType == "Playoffs") {
+                    if (game.game_type == "Playoffs") {
                         continue;       // Ignore playoff games until the games are added to the public calendars
                     }
 
-                    if (ageGroup == "Mentor") {
+                    if (age_group == "Mentor") {
                         // This is a placeholder for an umpire mentor assignment, it doesn't represent a game
                         continue;
                     }
 
-                    if (ageGroup == "13U A")
+                    if (age_group == "13U A")
                     {
                         homeTeam = "VCB 13U " + homeTeam;
 
@@ -169,64 +223,64 @@ namespace VcbFieldExport
                         }
                         // The visiting team is a non-org team, TeamSnap & Assignr should have the same name
                     }
-                    else if (ageGroup == "15U A")
+                    else if (age_group == "15U A")
                     {
-                        if (ASSIGNR_TO_PUBLIC_NAMEMAP[ageGroup].ContainsKey(homeTeam))
+                        if (ASSIGNR_TO_PUBLIC_NAMEMAP[age_group].ContainsKey(homeTeam))
                         {
-                            homeTeam = ASSIGNR_TO_PUBLIC_NAMEMAP[ageGroup][homeTeam];
+                            homeTeam = ASSIGNR_TO_PUBLIC_NAMEMAP[age_group][homeTeam];
                         }
                         else
                         {
                             homeTeam = "VCB 15U " + homeTeam;
                         }
 
-                        if (ASSIGNR_TO_PUBLIC_NAMEMAP[ageGroup].ContainsKey(awayTeam))
+                        if (ASSIGNR_TO_PUBLIC_NAMEMAP[age_group].ContainsKey(awayTeam))
                         {
-                            awayTeam = ASSIGNR_TO_PUBLIC_NAMEMAP[ageGroup][awayTeam];
+                            awayTeam = ASSIGNR_TO_PUBLIC_NAMEMAP[age_group][awayTeam];
                         }
                         else
                         {
                             awayTeam = "VCB 15U " + awayTeam;
                         }
                     }
-                    else if (ageGroup == "18U AA")
+                    else if (game.age_group == "18U AA")
                     {
                         homeTeam = "VCB 18U " + homeTeam;
                         awayTeam = "VCB 18U " + awayTeam;
                     }
-                    else if (ASSIGNR_TO_PUBLIC_NAMEMAP.ContainsKey(ageGroup))
+                    else if (ASSIGNR_TO_PUBLIC_NAMEMAP.ContainsKey(age_group))
                     {
-                        if (ASSIGNR_TO_PUBLIC_NAMEMAP[ageGroup].ContainsKey(homeTeam))
+                        if (ASSIGNR_TO_PUBLIC_NAMEMAP[age_group].ContainsKey(homeTeam))
                         {
-                            homeTeam = ASSIGNR_TO_PUBLIC_NAMEMAP[ageGroup][homeTeam];
+                            homeTeam = ASSIGNR_TO_PUBLIC_NAMEMAP[age_group][homeTeam];
                         }
 
-                        if (ASSIGNR_TO_PUBLIC_NAMEMAP[ageGroup].ContainsKey(awayTeam))
+                        if (ASSIGNR_TO_PUBLIC_NAMEMAP[age_group].ContainsKey(awayTeam))
                         {
-                            awayTeam = ASSIGNR_TO_PUBLIC_NAMEMAP[ageGroup][awayTeam];
+                            awayTeam = ASSIGNR_TO_PUBLIC_NAMEMAP[age_group][awayTeam];
                         }
                         // The visiting team is a non-VCB team, TeamSnap & Assignr should have the same name
                     }
                     else
                     {
-                        throw new Exception($"Unhandled age group {ageGroup} was sent by Assignr.  The program needs to be updated to handle this.");
+                        throw new Exception($"Unhandled age group {game.age_group} was sent by Assignr.  The program needs to be updated to handle this.");
                     }
 
-                    DateTime start = DateTime.Parse(startTime).ToUniversalTime();
-
-                    if (!ASSIGNR_TO_TEAMSNAP_VENUE_MAP.ContainsKey(assignrVenue))
+                    if (!ASSIGNR_TO_TEAMSNAP_VENUE_MAP.ContainsKey(game._embedded.venue.name))
                     {
-                        // mLogger.WriteLine($"Warning: A game in Assignr is hosted on a field not tracked publically: {assignrVenue} on {startTime}.");
+                        // mLogger.WriteLine($"Warning: A game in Assignr is hosted on a field not tracked publically: {game._embedded.venue.name} on {startTime}.");
                         continue;
                     }
 
-                    string teamSnapVenue = ASSIGNR_TO_TEAMSNAP_VENUE_MAP[assignrVenue];
+                    string teamSnapVenue = ASSIGNR_TO_TEAMSNAP_VENUE_MAP[game._embedded.venue.name];
+                    DateTime start = game.start_time.ToUniversalTime();
+
 
                     if (IGNORED_GAMES.Find(e => e.location == teamSnapVenue && e.startTime == start) != null) {
                         continue;
                     }
 
-                    mGames.Add(new VcbFieldEvent(teamSnapVenue, start, ageGroup, homeTeam, awayTeam, true));
+                    mGames.Add(new VcbFieldEvent(teamSnapVenue, start, game.age_group ?? string.Empty, homeTeam, awayTeam, true));
                 }
 
                 ++currentPage;
@@ -245,7 +299,7 @@ namespace VcbFieldExport
                     continue;
                 }
 
-                VcbFieldEvent e = mGames.Find(e =>
+                VcbFieldEvent? e = mGames.Find(e =>
                     e.location == game.location &&
                     e.startTime == game.startTime &&
                     e.homeTeam == game.homeTeam &&

VCB Field Loc Export/GoogleEvents.cs
@@ -112,7 +112,7 @@ namespace VcbFieldExport
 
         List<VcbFieldEvent> fetchEvents(string location, out CalendarService calendarService)
         {
-            Events events;
+            Google.Apis.Calendar.v3.Data.Events events;
 
             try {
                 calendarService = GetGoogleCalendarService(location);

VCB Field Loc Export/Program.cs
@@ -6,6 +6,7 @@ namespace VcbFieldExport
         static int Main(string[] args)
         {
             DateTime now = DateTime.Now;
+            int errors = 0;
 
             string logFileName = $"fieldSync.{now.Year}-{now.Month}-{now.Day}.log";
             StreamWriter logger = new StreamWriter(logFileName, false);
@@ -14,7 +15,7 @@ namespace VcbFieldExport
             teamSnap.FetchEvents();
 
             // Find conflicts in the game/practice schedule
-            int errors = teamSnap.FindConflicts();
+            errors += teamSnap.FindConflicts();
 
             string ASSIGNR_ID_LMB = "627";
             string ASSINGR_ID_VCB = "12381";

VCB Field Loc Export/SportsEngine.cs
@@ -1,14 +1,6 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using VcbFieldExport;
-using static Google.Apis.Requests.BatchRequest;
 
 namespace VcbFieldExport
 {
@@ -25,6 +17,91 @@ namespace VcbFieldExport
 
         [JsonProperty("client_secret")]
         public string Secret { get; set; }
+    }
+
+    internal class Data
+    {
+        public Data() {
+            events = new();
+        }
+
+        public Events events { get; set; }
+    }
+
+    internal class Events
+    {
+        public Events() {
+            pageInformation = new();
+            results = new();
+        }
+
+        public PageInformation pageInformation { get; set; }
+        public List<Result> results { get; set; }
+    }
+
+    internal class EventTeam
+    {
+        public EventTeam()
+        {
+            team = new();
+            homeTeam = true;
+        }
+
+        public Team team { get; set; }
+        public bool homeTeam { get; set; }
+    }
+
+    internal class Location
+    {
+        public Location() {
+            name = string.Empty;
+        }
+
+        public string name { get; set; }
+    }
+
+    internal class PageInformation
+    {
+        public int pages { get; set; }
+        public int count { get; set; }
+        public int page { get; set; }
+        public int perPage { get; set; }
+    }
+
+    internal class Result
+    {
+        Result()
+        {
+            name = string.Empty;
+            type = string.Empty;
+            location = new();
+            eventTeams = new();
+        }
+
+        public string name { get; set; }
+        public string type { get; set; }
+        public DateTime start { get; set; }
+        public DateTime end { get; set; }
+        public Location location { get; set; }
+        public List<EventTeam> eventTeams { get; set; }
+    }
+
+    internal class EventQueryResult
+    {
+        public EventQueryResult() {
+            data = new();
+        }
+
+        public Data data { get; set; }
+    }
+
+    internal class Team
+    {
+        public Team() {
+            name = string.Empty; 
+        }
+
+        public string name { get; set; }
     }
 
     internal class SportsEngine
@@ -88,13 +165,12 @@ namespace VcbFieldExport
             string results = @"results { name type start end location { name } eventTeams { team { name } homeTeam } } }";
 
             int pageNumber = 1;
-            int pageSize = 100;
             int eventCount = 0;
 
             while (true) {
 
                 // If we want to put these events on a public calendar, we'll need GAMEs and EVENTs for the calendarEventType
-                string query = $"query events {{ events( organizationId: {LMB_ORGANIZATION_ID} from: \\\"{DateTime.Today:O}\\\" perPage: {pageSize} page: {pageNumber} calendarEventType: GAME) {{ {pageInfo} {results} }}";
+                string query = $"query events {{ events( organizationId: {LMB_ORGANIZATION_ID} from: \\\"{DateTime.Today:O}\\\" perPage: 100 page: {pageNumber} calendarEventType: GAME) {{ {pageInfo} {results} }}";
 
                 string uri = "https://api.sportsengine.com/graphql";
                 HttpRequestMessage msg = new HttpRequestMessage(HttpMethod.Post, uri);
@@ -113,28 +189,20 @@ namespace VcbFieldExport
                 // bug because it's ignoring the "Z" suffix, which means the timestamp is in UTC, not local
                 JsonSerializerSettings settings = new();
                 settings.DateParseHandling = DateParseHandling.None;
-                JObject jsonRoot = JsonConvert.DeserializeObject(jsonResponse, settings) as JObject ?? new JObject();
-                JObject? pageInformation = jsonRoot["data"]["events"]["pageInformation"] as JObject;
+                EventQueryResult eventQueryResult = JsonConvert.DeserializeObject<EventQueryResult>(jsonResponse) ?? new EventQueryResult();
 
-                eventCount = (int)pageInformation["count"];
-                if (pageNumber != (int)pageInformation["page"]) {
+                eventCount = eventQueryResult.data.events.pageInformation.count;
+
+                if (pageNumber != eventQueryResult.data.events.pageInformation.page) {
                     throw new Exception("Unexpected page returned");
                 }
 
-                JArray eventList = jsonRoot["data"]["events"]["results"] as JArray;
-
-                foreach (JObject e in eventList) {
-                    string location = (string)e["location"]["name"];
-                    string strStartTime = (string)e["start"];
-                    DateTime startTime = DateTime.Parse(strStartTime).ToUniversalTime();
-                    string strEndTime = (string)e["start"];
-                    DateTime endTime = DateTime.Parse(strEndTime).ToUniversalTime();
-                    string homeTeam = (string)e["eventTeams"][0]["team"]["name"];
-                    string visitingTeam = (string)e["eventTeams"][1]["team"]["name"];
-                    string division = string.Empty;
+                foreach (Result e in eventQueryResult.data.events.results) {
+                    string homeTeam = e.eventTeams[0]?.team?.name ?? "TBD";
+                    string visitingTeam = e.eventTeams[1]?.team?.name ?? "TBD";
                     bool officialsRequired = homeTeam.StartsWith("MINB") || homeTeam.StartsWith("MINA") || homeTeam.StartsWith("MAJ");
 
-                    mGames.Add(new VcbFieldEvent(location, startTime, division, homeTeam, visitingTeam, officialsRequired));
+                    mGames.Add(new VcbFieldEvent(e.location.name ?? "TBD", e.start, string.Empty, homeTeam, visitingTeam, officialsRequired));
                 }
 
                 if (mGames.Count == eventCount) {

commit a0de17eaad1e4c14def03a4ab9ecc680874984ba

Fixed practice end times.
This bug was introduced in commit fa3315eae9e10f1545e171a1793b43218f45bdec (April 27, 2025), so it's been here for about 3 1/2 weeks before someone asked me about why practices were only showing up as being 30 minutes long.  I re-synced all the field calendars as a result of this error.

VCB Field Loc Export/TeamSnapEvents.cs
@@ -114,7 +114,7 @@ namespace VcbFieldExport
                     string location = e.data.Find(x => x.name == "location_name")?.value ?? "TBD";
                     DateTime startTime = DateTime.Parse(e.data.Find(x => x.name == "start_date")?.value ?? string.Empty).ToUniversalTime();
                     startTime = startTime.AddSeconds(-startTime.Second);    // sometimes TeamSnap events have non-zero seconds values, not sure why
-                    string? endDateString = e.data.Find(x => x.name == "start_date")?.value;
+                    string? endDateString = e.data.Find(x => x.name == "end_date")?.value;
                     DateTime endTime = (endDateString != null) ? DateTime.Parse(endDateString).ToUniversalTime() : startTime.AddHours(2);
                     endTime = endTime.AddSeconds(-endTime.Second);
                     string formatted_title = e.data.Find(x => x.name == "formatted_title")?.value ?? string.Empty;