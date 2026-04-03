66891cb4760b591685943cb4f3b0e16530274640
James Fairweather <james.d.fairweather@gmail.com>
2026-04-03 10:50:00 -0700

    Added new project "Shared", which will contain classes common to both programs

---
+++ Shared/Credentials.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+//namespace Shared
+//{
+    public class Credentials
+    {
+        [JsonProperty("teamsnap_bearer_token")]
+        // TODO: Figure out how to get a bearer token from TeamSnap's service using the client ID & secret.
+        // Fortunately, the bearer token seems to be perpertual so we can just put that into our credential file
+        // until the above TODO is done.
+        public string? Teamsnap { get; set; }
+        public Google.Apis.Auth.OAuth2.ClientSecrets? Sportsengine { get; set; }
+        public Google.Apis.Auth.OAuth2.ClientSecrets? Assignr { get; set; }
+        public Google.Apis.Auth.OAuth2.ClientSecrets? Google { get; set; }
+    }
+//}
