07a296165ea18cbf2b45d804d9ba3b6a89ed6811
James Fairweather <james.d.fairweather@gmail.com>
2026-04-03 11:21:01 -0700

    Add Token class to the shared project.

---
+++ Shared/Token.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+public class Token
+{
+    Token()
+    {
+        AccessToken = string.Empty;
+        TokenType = string.Empty;
+        ExpiresIn = 0;
+        Scope = string.Empty;
+        Created = 0;
+    }
+
+    [JsonProperty("access_token")]
+    public string AccessToken { get; set; }
+
+    [JsonProperty("token_type")]
+    public string TokenType { get; set; }
+
+    [JsonProperty("expires_in")]
+    public int ExpiresIn { get; set; }
+
+    [JsonProperty("scope")]
+    public string Scope { get; set; }
+    [JsonProperty("created_at")]
+    public int Created { get; set; }
+}
