286d8e921f6290931f1294a6c257895539ca221d
James Fairweather <james.d.fairweather@gmail.com>
2026-04-03 11:22:04 -0700

    FieldSync project uses the Token class from the Shared project

--- VCB Field Loc Export/AssignrEvents.cs
+++ VCB Field Loc Export/AssignrEvents.cs
@@ -6,32 +6,6 @@ using static Google.Apis.Requests.BatchRequest;
 
 namespace VcbFieldExport
 {
-    internal class Token
-    {
-        Token()
-        {
-            AccessToken = string.Empty;
-            TokenType = string.Empty;
-            ExpiresIn = 0;
-            Scope = string.Empty;
-            Created = 0;
-        }
-
-        [JsonProperty("access_token")]
-        public string AccessToken { get; set; }
-
-        [JsonProperty("token_type")]
-        public string TokenType { get; set; }
-
-        [JsonProperty("expires_in")]
-        public int ExpiresIn { get; set; }
-
-        [JsonProperty("scope")]
-        public string Scope { get; set; }
-        [JsonProperty("created_at")]
-        public int Created { get; set; }
-    }
-
     internal class Game
     {
         public Game()
