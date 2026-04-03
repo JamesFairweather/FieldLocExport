adff851d7f7e43609b7207ae20dcec3b43afb626
James Fairweather <james.d.fairweather@gmail.com>
2026-04-03 11:09:47 -0700

    Retarget the FieldSync program to .NET 10, add a reference to the Shared project, use the Shared project's Credential class

--- VCB Field Loc Export/FieldSync.cs
+++ VCB Field Loc Export/FieldSync.cs
@@ -3,18 +3,6 @@ using Newtonsoft.Json;
 
 namespace VcbFieldExport
 {
-    public class Credentials
-    {
-        [JsonProperty("teamsnap_bearer_token")]
-        // TODO: Figure out how to get a bearer token from TeamSnap's service using the client ID & secret.
-        // Fortunately, the bearer token seems to be perpertual so we can just put that into our credential file
-        // until the above TODO is done.
-        public string? Teamsnap { get; set; }
-        public Google.Apis.Auth.OAuth2.ClientSecrets? Sportsengine { get; set; }
-        public Google.Apis.Auth.OAuth2.ClientSecrets? Assignr { get; set; }
-        public Google.Apis.Auth.OAuth2.ClientSecrets? Google { get; set; }
-    }
-
     public class Field
     {
         public Field(DateTime start, DateTime end, string teamSnapId, string googleID)
