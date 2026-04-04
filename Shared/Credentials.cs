7503d09f92e6b3546d718e1c0f1e18e9098a76cd
James Fairweather <james.d.fairweather@gmail.com>
2026-04-04 03:32:18 -0700

    Add Assignr web session token to the credentials file.  Sad that this is necessary, but there's no way using the API to retrieve game requests, so we have to use a web request.  At least it returns a response in JSON format

--- Shared/Credentials.cs
+++ Shared/Credentials.cs
@@ -2,11 +2,22 @@
 
 public class Credentials
 {
+    public Credentials()
+    {
+        AssignrSessionToken = string.Empty;
+    }
+
     [JsonProperty("teamsnap_bearer_token")]
     // TODO: Figure out how to get a bearer token from TeamSnap's service using the client ID & secret.
     // Fortunately, the bearer token seems to be perpertual so we can just put that into our credential file
     // until the above TODO is done.
     public string? Teamsnap { get; set; }
+
+    [JsonProperty("assignr_session_token")]
+    // Get this from a web browser after signing into Assignr.  IDK how long it's valid, but I really only
+    // need it once a week.  Still a lot faster than pulling the game requests from a browser session into
+    // a spreadsheet so I can analyze them.
+    public string AssignrSessionToken { get; set; }
     public Google.Apis.Auth.OAuth2.ClientSecrets? Sportsengine { get; set; }
     public Google.Apis.Auth.OAuth2.ClientSecrets? Assignr { get; set; }
     public Google.Apis.Auth.OAuth2.ClientSecrets? Google { get; set; }
